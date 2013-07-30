ab37727499261c5723d32b61d4f54689f493dff5
Sujay Mansingh <sujay@editd.com>
2013-07-30 13:24:34 +0100

    Bump python version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             "django_application"
 maintainer       "dev"
 maintainer_email "dev@editd.com"
-version          "0.0.7"
+version          "0.0.8"
 
 depends "gunicorn", "= 1.1.0"
 depends "nginx", "= 1.7.0"
-depends "python", "= 1.3.0"
+depends "python", "= 1.3.2"
 depends "supervisord", "= 0.1.0"
